6feb52a404395aac4a496e129713523ddeb33af4
thomaslienbacher <lienbacher.tom@gmail.com>
2020-05-10 02:16:04 +0200

    Refactor and improvements
    Removed Color struct, made Bounds into a seperate struct, zoom and translate now use the frame time to calculate change, delta time is now in the title and code reformating

--- src/fractal.rs
+++ src/fractal.rs
@@ -1,110 +1,95 @@
-pub mod color {
+use palette::{LinSrgb};
+use num::complex::Complex64;
+use interpolation::lerp;
 
-    pub struct Color {
-        r: f64,
-        g: f64,
-        b: f64,
-        a: f64,
-    }
+pub trait ColorEncode {
+    fn encode(&self) -> u32;
+}
 
-    impl Color {
-        pub const fn new() -> Color {
-            Color {
-                r: 0.,
-                g: 0.,
-                b: 0.,
-                a: 1.,
-            }
-        }
+impl ColorEncode for LinSrgb {
+    fn encode(&self) -> u32 {
+        let r = (self.red * 255.0) as u8;
+        let g = (self.green * 255.0) as u8;
+        let b = (self.blue * 255.0) as u8;
 
-        pub const fn rgb(r: f64, g: f64, b: f64) -> Color {
-            Color {
-                r,
-                g,
-                b,
-                a: 1.,
-            }
-        }
-    }
-
-    impl std::convert::From<Color> for u32 {
-        fn from(c: Color) -> Self {
-            let r = num::clamp((c.r * 255.0) as u8, 0, 255);
-            let g = num::clamp((c.g * 255.0) as u8, 0, 255);
-            let b = num::clamp((c.b * 255.0) as u8, 0, 255);
-            let a = num::clamp((c.a * 255.0) as u8, 0, 255);
-
-            ((a as u32) << 24) | ((r as u32) << 16) | ((g as u32) << 8) | b as u32
-        }
+        (255 << 24) | ((r as u32) << 16) | ((g as u32) << 8) | b as u32
     }
 }
 
-pub mod julia {
-    use num::complex::Complex64;
-    use interpolation::lerp;
+#[derive(Debug)]
+pub struct Bounds {
+    xbounds: (f64, f64),
+    ybounds: (f64, f64),
+}
 
-    #[derive(Debug)]
-    pub struct JuliaFractal {
-        pub xbounds: (f64, f64),
-        pub ybounds: (f64, f64),
-        pub add: Complex64,
-        pub max_iterations: u32,
+impl Bounds {
+    pub fn new(lower: f64, upper: f64, aspect_ratio_xy: f64) -> Bounds {
+        Bounds {
+            xbounds: (lower * aspect_ratio_xy, upper * aspect_ratio_xy),
+            ybounds: (lower, upper),
+        }
     }
 
-    impl JuliaFractal {
-        pub fn new() -> JuliaFractal {
-            JuliaFractal {
-                xbounds: (-1.7, 1.7),
-                ybounds: (-1.7, 1.7),
-                add: Complex64::new(0., 0.),
-                max_iterations: 60,
+    pub fn zoom(&mut self, mut x: f64, mut y: f64, delta_time: f64) {
+        x = 1. - x;
+        y = 1. - y;
+
+        let scale_x = (self.xbounds.1 - self.xbounds.0) * delta_time;
+        let scale_y = (self.ybounds.1 - self.ybounds.0) * delta_time;
+
+        self.xbounds.0 += (1. - x) * scale_x;
+        self.xbounds.1 -= x * scale_x;
+        self.ybounds.0 += (1. - y) * scale_y;
+        self.ybounds.1 -= y * scale_y;
+    }
+
+    pub fn translate(&mut self, mut x: f64, mut y: f64, delta_time: f64) {
+        let scale = ((self.xbounds.1 - self.xbounds.0) + (self.ybounds.1 - self.ybounds.0))
+            * delta_time;
+
+        x -= 0.5;
+        y -= 0.5;
+        x *= scale;
+        y *= scale;
+
+        self.xbounds.0 += x;
+        self.xbounds.1 += x;
+        self.ybounds.0 += y;
+        self.ybounds.1 += y;
+    }
+}
+
+#[derive(Debug)]
+pub struct JuliaFractal {
+    pub bounds: Bounds,
+    pub add: Complex64,
+}
+
+impl JuliaFractal {
+    pub const fn new(bounds: Bounds) -> JuliaFractal {
+        JuliaFractal {
+            bounds,
+            add: Complex64::new(-0., 0.),
+        }
+    }
+
+    fn f(&self, z: Complex64) -> Complex64 {
+        z.powu(2) + self.add
+    }
+
+    pub fn get(&self, x: f64, y: f64, max_iterations: u32) -> f64 {
+        let mut z = Complex64::new(
+            lerp(&self.bounds.xbounds.0, &self.bounds.xbounds.1, &x),
+            lerp(&self.bounds.ybounds.0, &self.bounds.ybounds.1, &y));
+
+        for iteration in 0..=max_iterations {
+            if (z.re * z.re) + (z.im * z.im) >= 4. {
+                return iteration as f64;
             }
+
+            z = self.f(z);
         }
 
-        fn f(&self, z: Complex64) -> Complex64 {
-            z.powu(2) + self.add
-        }
-
-        pub fn get(&self, x: f64, y: f64) -> f64 {
-            let mut z = Complex64::new(
-                lerp(&self.xbounds.0, &self.xbounds.1, &x),
-                lerp(&self.ybounds.0, &self.ybounds.1, &y));
-
-            for iteration in 0..=self.max_iterations {
-                if (z.re * z.re) + (z.im * z.im) >= 4. {
-                    return iteration as f64;
-                }
-
-                z = self.f(z);
-            }
-
-            return self.max_iterations as f64;
-        }
-
-        pub fn zoom(&mut self, mut x: f64, mut y: f64, scale: f64) {
-            const ZOOM: f64 = 0.07;
-            x = 1. - x;
-            y = 1. - y;
-
-            let scale_x = (self.xbounds.1 - self.xbounds.0) * scale;
-            let scale_y = (self.ybounds.1 - self.ybounds.0) * scale;
-
-            self.xbounds.0 += (1. - x) * ZOOM * scale_x;
-            self.xbounds.1 -= x * ZOOM * scale_x;
-            self.ybounds.0 += (1. - y) * ZOOM * scale_y;
-            self.ybounds.1 -= y * ZOOM * scale_y;
-        }
-
-        pub fn translate(&mut self, mut x: f64, mut y: f64) {
-            const SCALE: f64 = 0.1;
-            let mut scale = (self.xbounds.1 - self.xbounds.0) + (self.ybounds.1 - self.ybounds.0);
-            scale *= 0.9;
-            x -= 0.5;
-            y -= 0.5;
-            self.xbounds.0 += x * SCALE * scale;
-            self.xbounds.1 += x * SCALE * scale;
-            self.ybounds.0 += y * SCALE * scale;
-            self.ybounds.1 += y * SCALE * scale;
-        }
+        return max_iterations as f64;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,92 +1,86 @@
 mod fractal;
 
 use minifb::{Key, Window, WindowOptions, MouseButton, MouseMode, Scale, KeyRepeat};
-use crate::fractal::color::*;
-use crate::fractal::julia::JuliaFractal;
+use crate::fractal::*;
 use palette::{LinSrgb, Hsv, Gradient};
 use rayon::prelude::*;
 use num::complex::Complex64;
 use std::time::{SystemTime};
 
-const WIDTH: usize = 1920;
+const WIDTH: usize = 1200;
 const HEIGHT: usize = WIDTH / 16 * 9;
 
 fn main() {
     let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
-    let mut options = WindowOptions::default();
-    options.scale = Scale::FitScreen;
-    options.borderless = true;
-    options.resize = false;
-
+    let title = "View Fractals - ESC to exit";
     let mut window = Window::new(
-        "View Fractals - ESC to exit",
+        title,
         WIDTH,
         HEIGHT,
-        options,
+        WindowOptions {
+            scale: Scale::X1,
+            ..WindowOptions::default()
+        },
     ).unwrap_or_else(|e| {
-        panic!("{}", e);
+        panic!("Error: {}", e);
     });
 
-    let mut julia = JuliaFractal::new();
-    julia.xbounds.0 *= (WIDTH as f64 / HEIGHT as f64);
-    julia.xbounds.1 *= (WIDTH as f64 / HEIGHT as f64);
-    let mut first = true;
+    let bounds = Bounds::new(-2.1, 2.1, WIDTH as f64 / HEIGHT as f64);
+    let mut julia = JuliaFractal::new(bounds);
+    let mut max_iterations = 50;
+    let mut delta_time = 0.;
 
     let grad = Gradient::new(vec![
         Hsv::from(LinSrgb::new(1. as f32, 0. as f32, 0. as f32)),
         Hsv::from(LinSrgb::new(0. as f32, 1. as f32, 0. as f32)),
-        Hsv::from(LinSrgb::new(0. as f32, 0. as f32, 1. as f32))
+        Hsv::from(LinSrgb::new(0. as f32, 0. as f32, 1. as f32)),
+        Hsv::from(LinSrgb::new(1. as f32, 1. as f32, 1. as f32))
     ]);
 
     println!("{:?}", julia);
 
     while window.is_open() && !window.is_key_released(Key::Escape) {
-        if first {
-            let start = SystemTime::now();
-            buffer.par_iter_mut().enumerate().for_each(|(i, v)| {
-                let x = ((i % WIDTH) as f64 / WIDTH as f64);
-                let y = ((i / WIDTH) as f64 / HEIGHT as f64);
-                let j = julia.get(x, y);
+        let start = SystemTime::now();
+        buffer.par_iter_mut().enumerate().for_each(|(i, v)| {
+            let x = ((i % WIDTH) as f64 / WIDTH as f64);
+            let y = ((i / WIDTH) as f64 / HEIGHT as f64);
+            let j = julia.get(x, y, max_iterations);
 
-                let g = {
-                    let m = julia.max_iterations - 1;
-                    let p = (j as f32) / m as f32;
-                    p
-                };
+            let g = {
+                let m = max_iterations - 1;
+                let p = (j as f32) / m as f32;
+                p
+            };
 
-                let c: LinSrgb = if j != julia.max_iterations as f64 {
-                    LinSrgb::from(grad.get(g))
-                } else {
-                    LinSrgb::new(0., 0., 0.)
-                };
+            if j != max_iterations as f64 {
+                *v = LinSrgb::from(grad.get(g)).encode();
+            } else {
+                *v = 0;
+            };
+        });
 
-                *v = Color::rgb(c.red.into(), c.green.into(), c.blue.into()).into();
-            });
-
-            let time_passed = SystemTime::now().duration_since(start).unwrap();
-            println!("Frame time: {}", time_passed.as_secs_f32());
-
-            //first = false;
-        }
+        let time_passed = SystemTime::now().duration_since(start).unwrap();
+        delta_time = time_passed.as_secs_f64();
+        window.set_title(format!("{} DT: {}", title, delta_time).as_ref());
 
         if window.is_key_pressed(Key::W, KeyRepeat::Yes) {
-            julia.max_iterations += 1;
+            max_iterations += 1;
         }
         if window.is_key_pressed(Key::S, KeyRepeat::Yes) {
-            if julia.max_iterations > 2 {
-                julia.max_iterations -= 1;
+            if max_iterations > 2 {
+                max_iterations -= 1;
             }
         }
 
         if let Some((a, b)) = window.get_mouse_pos(MouseMode::Discard) {
             if window.get_mouse_down(MouseButton::Left) {
-                julia.zoom(a as f64 / WIDTH as f64, b as f64 / HEIGHT as f64, 1.0);
+                julia.bounds.zoom(a as f64 / WIDTH as f64, b as f64 / HEIGHT as f64, delta_time);
             }
             if window.get_mouse_down(MouseButton::Right) {
-                julia.zoom(a as f64 / WIDTH as f64, b as f64 / HEIGHT as f64, -1.0);
+                julia.bounds.zoom(a as f64 / WIDTH as f64, b as f64 / HEIGHT as f64, -delta_time);
             }
             if window.get_mouse_down(MouseButton::Middle) {
-                julia.translate(a as f64 / WIDTH as f64, b as f64 / HEIGHT as f64);
+                julia.bounds.translate(a as f64 / WIDTH as f64, b as f64 / HEIGHT as f64, delta_time);
             }
             if window.is_key_down(Key::Space) {
                 julia.add = Complex64::new((a as f64 / WIDTH as f64) - 0.5, (b as f64 / HEIGHT as f64) - 0.5);
@@ -95,7 +89,7 @@ fn main() {
             }
         }
 
-        //println!("{:?}", julia);
+        println!("{:?}", julia);
 
         window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
     }
